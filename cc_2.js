c304f4104925b3fcc5fde1a21d42d1ae6cf09cd3
Divya Joshi <divyaj.redj@gmail.com>
2025-01-23 15:53:18 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,11 @@
+//Task 1: Declaring Tasks with an Array//
+let products = ["Apples", "Bananas", "Carrots", "Detergent", "Eggs"];
+
+// Add a new product to the array
+products.push("Flour");
+
+// Remove the last product from the array
+products.pop();
+
+//log in console//
+console.log('Updated product lists:', products)
