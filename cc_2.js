90c21c0cf659957824fd24a78322a0be30c1b3b6
Divya Joshi <divyaj.redj@gmail.com>
2025-01-23 16:05:46 -0500

    Task 2 - Student Scores."

--- cc_2.js
+++ cc_2.js
@@ -9,3 +9,15 @@ products.pop();
 
 //log in console//
 console.log('Updated product lists:', products)
+
+//Task 2//
+let scores = [85,95,76,88,92];//declaring array with numerical values//
+
+scores[1] = 95; //updated second score in array//
+
+//Calculating scores//
+let total = scores.reduce((sum, score) => sum + score, 0);
+let average = total / scores.length;
+
+console.log("Updated Scores:", scores);
+console.log("Average Score:", average);
